68ce7b6b7a75ab187274bdb23d45ac2dc0bdaa2c
gusta <guusfelix2015>
2021-03-20 23:31:51 -0300

    Initial commmit

---
+++ ex1.js
@@ -0,0 +1,20 @@
+const nome = "Gustavo";
+let idade = 25;
+
+let comidaFavorita;
+comidaFavorita = "Lasanha";
+
+let cor;
+let cidade;
+let celular;
+let cabelo;
+let data;
+
+console.log(nome);
+console.log(idade);
+console.log(comidaFavorita);
+console.log(cor);
+console.log(cidade);
+console.log(celular);
+console.log(cabelo);
+console.log(data);
---
+++ script.js
@@ -0,0 +1,24 @@
+var nome = "André";
+var idade = 28;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = "Rafael",
+  cidade = "Rio";
+
+console.log(sobrenome, cidade);
+
+var semDefenir;
+
+console.log(semDefenir);
+
+var time = "Vasco";
+time = "Flamengo";
+console.log(time);
