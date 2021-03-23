5483a3ef3f40971026a0bb70fd5588fade7e0f2a
gusta <guusfelix2015>
2021-03-22 22:00:36 -0300

    tipos de dados

---
+++ tiposDeDados/dados.js
@@ -0,0 +1,9 @@
+var idade = 28;
+var simbolo = Symbol();
+console.log(typeof simbolo);
+
+// template string
+
+var gols = 1000;
+var frase1 = `Romário fez ${gols * 2} gols`;
+console.log(frase1);
---
+++ tiposDeDados/ex2.js
@@ -0,0 +1,21 @@
+// Declare uma variável contendo uma string
+var strind = "String";
+// Declare uma variável contendo uma número dentro de uma string
+var numString = "2018";
+// Declare uma variável com a sua idade
+var minhaIdade = 25;
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenime e some os mesmos
+var nome = "Gustavo";
+var sobreNome = "Felix";
+
+var nomeCompleto = `${nome} ${sobreNome}`;
+
+// Coloque a seguinte frase em uma variável: It's time
+
+var frasee = "It's time";
+
+// Verigique o tipo de variável que contém o seu nome
+
+console.log(nomeCompleto);
+console.log(frasee);
