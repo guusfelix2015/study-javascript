cfe38653e7cdce03a2e0b5c1223457161ba5ba96
gusta <guusfelix2015>
2021-03-24 18:16:07 -0300

    operator

---
+++ operadores/number.js
@@ -0,0 +1,25 @@
+var total = 10 + (5 * 2) / 2 + 20; //35
+
+// Crie duas expressões que retornam NaN
+var nan = "Teste" / 2;
+
+// Somar a string '200' com o número 50 e retornar 250
+const string = "200";
+const number = 50;
+const toral1 = +string + 50;
+
+// Incrementar o número 5 e retorne o seu valor incrementado
+
+var y = 5;
+
+// Como dividir o peso por 2 ?
+
+var numero = +"80" / 2;
+var unidade = "kg";
+var peso = numero + unidade;
+
+console.log(total);
+console.log(nan);
+console.log(toral1);
+console.log(peso);
+console.log(++y);
