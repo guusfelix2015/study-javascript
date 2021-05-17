0d57830df5cf71a5f9ea52890bc5f31be5dbc9e5
unknown <guusfelix2015>
2021-05-17 16:35:19 -0300

    iniciando nos loops

---
+++ arrays/arrays.js
@@ -0,0 +1,10 @@
+var videogames = ["Switch", "PS4", "EXBOX"];
+
+var ultimoItem = videogames.pop();
+videogames.push("3DS");
+
+// loops
+
+for (var numero = 0; numero < 10; numero++) {
+  console.log(numero);
+}
